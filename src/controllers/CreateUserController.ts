0a630251b057bd20faa8604fba945f81cfb049ec
Igor Monteiro <igornathandev@gmail.com>
2021-09-07 07:48:08 -0300

    Third class

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -4,13 +4,14 @@ import { CreateUserService } from '../services/CreateUserSerice';
 class CreateUserController {
 
   async handle(request: Request, response: Response) {
-    const { name, email, admin } = request.body;
+      const { name, email, admin } = request.body;
+  
+      const createUserSerice = new CreateUserService();
+  
+      const user = await createUserSerice.execute({ name, email, admin });
+  
+      return response.json(user);    
 
-    const createUserSerice = new CreateUserService();
-
-    const user = await createUserSerice.execute({ name, email, admin });
-
-    return response.json(user)
   }
 }
 
